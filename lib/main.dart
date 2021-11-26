29d5599521862d474bd941ec785207b1050ef2c6
KanamoriKouhei <glglntn6@gmail.com>
2021-11-27 00:13:31 +0900

    なんでtextDirection書かないとエラーになるのかわからん

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const Center(
+      child: Text(
+    'HELLO WORLD',
+    textDirection: TextDirection.ltr,
+  )));
 }
 
-//アプリ本体
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+// //アプリ本体
+// class App extends StatelessWidget {
+//   const App({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MaterialApp',
-      theme: ThemeData.dark(),
-    );
-  }
-}
-//Home画面
-class Home extends StatefulWidget {
-  Home({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'MaterialApp',
+//       theme: ThemeData.dark(),
+//     );
+//   }
+// }
 
-  @override
-  _HomeState createState() => _HomeState();
-}
+// //Home画面
+// class Home extends StatefulWidget {
+//   Home({Key? key}) : super(key: key);
 
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
